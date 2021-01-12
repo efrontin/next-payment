941f19da3aa1617d611af3e0de0e3d5fd28ccaf4
Evrard FRONTIN <fe.moustic@gmail.fr>
2021-01-12 10:15:33 +0100

    [CONNEXION API] update submit data

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Form, Field } from 'react-final-form'
 import Card from '../components/shared/Card'
 import {
@@ -8,21 +8,58 @@ import {
     formatCVC,
     formatExpirationDate
 } from '../components/shared/cardUtils'
-import PaymentView from "../components/shared/paymentForm";
 import Styles from "../components/shared/Styles";
 import {Button, makeStyles} from "@material-ui/core";
+import Axios from "axios";
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-const onSubmit = async values => {
-    console.log("INFORMATIONS PAYMENT", values)
-    await sleep(300)
-    window.alert(JSON.stringify(values, 0, 2))
-}
-
+// useEffect(() => {
+    // fetch('')
+    //     .then( res => {
+    //         if (!res.ok) {
+    //             throw Error('could not send the data')
+    //         }
+    //         return res.json();
+    //     })
+    //     .then( data => {
+    //         console.log(data)
+    //     })
+    //     .catch( err => {
+    //         console.log(err.message)
+    //     })
+// }, [])
 
 
 export default function Home() {
+
+
+    const [isPending, setIsPending] = useState(false)
+    const [error, setError] = useState(null)
+
+    const onSubmit = async values => {
+
+
+        console.log("INFORMATIONS PAYMENT", values.amount, values.libelle, values)
+        await sleep(300)
+        window.alert(JSON.stringify(values, 0, 2))
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(values)
+        };
+
+        await Axios.put('http://localhost:3000/api/hello', requestOptions)
+            .then(response => {
+                setIsPending(false)
+                this.state({amount: response.data.amount, libelle: response.data.libelle})
+            })
+            .catch( err => {
+                console.log(err)
+                setError(err.message)
+            })
+    }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -51,6 +88,8 @@ export default function Home() {
                                   number={values.number || ''}
                                   name={values.name || ''}
                                   expiry={values.expiry || ''}
+                                  amount={values.amount || ''}
+                                  libelle={values.libelle || ''}
                                   cvc={values.cvc || ''}
                                   focused={active}
                               />
@@ -104,9 +143,11 @@ export default function Home() {
                                       placeholder="Amount"
                                   />
                               </div>
+                              <div>{ error && <div>{ error }</div>}</div>
+                              <div>{ isPending && <div>Sending...</div>}</div>
                               <div className="buttons">
                                   <Button color="primary" type="submit" disabled={submitting}>
-                                      Submit
+                                      Envoyer
                                   </Button>
                                   <button
                                       type="button"
