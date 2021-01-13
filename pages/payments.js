65f3e503d905eefa7fdd75422a502c0ff3a084de
Maxime ALFOSEA <maxime.alfosea@hotmail.fr>
2021-01-13 10:44:54 +0100

    fix data grid errors

--- pages/payments.js
+++ pages/payments.js
@@ -3,6 +3,12 @@ import { DataGrid } from '@material-ui/data-grid';
 import axios from "axios";
 import { makeStyles } from '@material-ui/core/styles';
 
+const statusEnum = new Map([
+    [0, "UNKNOW"],
+    [1, "PENDING"],
+    [2, "ACCEPTED"],
+    [3, "VALIDATED"]
+])
 
 const useStyles = makeStyles({
     paymentsGrid: {
@@ -25,27 +31,42 @@ export default  function Payments({payments}) {
     return (
         <div style={{ height: 400, width: '100%' }}>
             <DataGrid className={classes.paymentsGrid}
-                columns={[{ field: '_id', headerName: 'Opération', flex: 0.5, headerClassName: 'paymentsGridHeader',
-                    headerAlign: 'center'},
-                    { field: 'label', headerName: 'Libellé', flex: 1, headerClassName: 'paymentsGridHeader',
-                        headerAlign: 'center' },
-                    { field: 'amount', headerName: 'Montant', flex: 0.5, headerClassName: 'paymentsGridHeader',
-                        headerAlign: 'center', cellClassName: 'paymentsGridCell' },
-                    { field: 'status', headerName: 'Status', flex: 0.5, headerClassName: 'paymentsGridHeader',
-                        headerAlign: 'center', cellClassName: 'paymentsGridCell' },
-                    { field: 'transactionDate', headerName: 'Date du traitement', flex: 1, headerClassName: 'paymentsGridHeader',
-                        headerAlign: 'center', cellClassName: 'paymentsGridCell' }]}
-                rows={payments}
+                      id='id'
+                      columns={[{ field: 'id', headerName: 'Opération', flex: 0.5, headerClassName: 'paymentsGridHeader',
+                        headerAlign: 'center', cellClassName: 'MuiDataGrid-cellCenter'},
+                        { field: 'label', headerName: 'Libellé', flex: 1, headerClassName: 'paymentsGridHeader',
+                            headerAlign: 'center' },
+                        { field: 'amount', headerName: 'Montant', flex: 0.5, headerClassName: 'paymentsGridHeader',
+                            headerAlign: 'center', cellClassName: 'MuiDataGrid-cellCenter' },
+                        { field: 'status', headerName: 'Status', flex: 0.5, headerClassName: 'paymentsGridHeader',
+                            headerAlign: 'center', cellClassName: 'MuiDataGrid-cellCenter' },
+                        { field: 'transactionDate', headerName: 'Date du traitement', flex: 1, headerClassName: 'paymentsGridHeader',
+                            headerAlign: 'center', cellClassName: 'MuiDataGrid-cellCenter' }]}
+                      rows={payments}
+                      sortingOrder={['desc', 'asc']}
+                      sortModel={[
+                          {
+                              field: 'transactionDate',
+                              sort: 'desc',
+                          }
+                      ]}
             />
         </div>
     )
 }
 
-
 export async function getServerSideProps() {
     const res = await axios.get("http://localhost:3000/api/payment");
+    const payments = res.data.map(
+        (payment, id) => {
+            return {
+                ...payment,
+                id : id + 1,
+                transactionDate : new Date(payment.transactionDate).toString(),
+                status : statusEnum.get(payment.status)
+            }
+        })
 
-    const payments = await res.data
     console.log(payments)
 
     return {
