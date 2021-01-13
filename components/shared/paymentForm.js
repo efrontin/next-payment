fe4c1366d9d451f88b255f895e13ed488bc785bb
Maxime ALFOSEA <maxime.alfosea@hotmail.fr>
2021-01-13 09:58:29 +0100

    add Evrard changes

--- components/shared/paymentForm.js
+++ components/shared/paymentForm.js
@@ -1,18 +1,10 @@
-import React from 'react';
-import * as Yup from 'yup';
-import { Formik } from 'formik';
-import {
-    Box,
-    Button,
-    Checkbox,
-    Container,
-    FormHelperText,
-    Link,
-    TextField,
-    Typography,
-    makeStyles
-} from '@material-ui/core';
-
+import React, {useEffect, useState} from 'react'
+import {Button, makeStyles} from "@material-ui/core";
+import Axios from "axios";
+import TextField from "@material-ui/core/TextField";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -23,92 +15,148 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-const PaymentView = () => {
+export default function Payment() {
     const classes = useStyles();
 
-    return (
-        <Box
-            display="flex"
-            flexDirection="column"
-            height="100%"
-            justifyContent="center"
-        >
-            <Container maxWidth="sm">
-                <Formik
-                    initialValues={{
-                        amount: '',
-                        receiver: '',
-                    }}
-                    validationSchema={
-                        Yup.object().shape({
-                            receiver: Yup.string().max(255).required('Receiver is required'),
-                            amount: Yup.number().positive().min(0,50).max(9999,99).required('Amount is required')
-                        })
-                    }
-                    onSubmit={() => {
-                        navigate('/api/success', { replace: true });
-                    }}
-                >
-                    {({
-                          errors,
-                          handleBlur,
-                          handleChange,
-                          handleSubmit,
-                          isSubmitting,
-                          touched,
-                          values
-                      }) => (
-                        <form onSubmit={handleSubmit}>
-                            <Box mb={3}>
-                                <Typography
-                                    color="textPrimary"
-                                    variant="h2"
-                                >
-                                    Send your payment
-                                </Typography>
-                            </Box>
-                            <TextField
-                                error={Boolean(touched.receiver && errors.receiver)}
-                                fullWidth
-                                helperText={touched.receiver && errors.receiver}
-                                label="Receiver"
-                                margin="normal"
-                                name="receiver"
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                value={values.receiver}
-                                variant="outlined"
-                            />
-                            <TextField
-                                error={Boolean(touched.amount && errors.amount)}
-                                fullWidth
-                                helperText={touched.amount && errors.amount}
-                                label="Amount"
-                                margin="normal"
-                                name="amount"
-                                onBlur={handleBlur}
-                                onChange={handleChange}
-                                value={values.amount}
-                                variant="outlined"
-                            />
-                            <Box my={2}>
-                                <Button
-                                    color="primary"
-                                    disabled={isSubmitting}
-                                    fullWidth
-                                    size="large"
-                                    type="submit"
-                                    variant="contained"
-                                >
-                                    Send
-                                </Button>
-                            </Box>
-                        </form>
-                    )}
-                </Formik>
-            </Container>
-        </Box>
-    );
-};
+    const [cardNumber, setCardNumber] = useState("")
+    const [amount, setAmount] = useState("")
+    const [label, setLabel] = useState("")
 
-export default PaymentView;
+    const [isPending, setIsPending] = useState(false)
+    const [amountErr, setAmountError] = useState({})
+    const [cardNumberErr, setCardNumberError] = useState({})
+    const [error, setError] = useState(null)
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const isValid = formValidation()
+        console.log()
+    }
+
+    const convertPrice = amount => {
+        (Math.round(amount * 1 * 100) / 100).toFixed(2);
+    }
+    const formValidation = () => {
+        const cardNumberErr = {}
+        const amountErr = {}
+        let isValid = true;
+
+        if (cardNumber.trim().length !== 16) {
+            cardNumberErr.cardNumberValidation = "Le nombre de numero sur" +
+                " la carte doit être de 16 chiffres";
+            isValid = false
+        }
+
+        // if (cardNumber.trim() !== toString()) {
+        //     cardNumberErr.cardNumberValidation = "Le numero de carte ne doit" +
+        //         " comporter que des chiffres";
+        //     isValid = false
+        // }
+
+        if (amount.trim().length < 10000) {
+            amountErr.amountTooLong = "Le montant doit être inferieur à" +
+                " 10000 €";
+            isValid = false
+        }
+
+        if (amount.trim().length > 0.5) {
+            amountErr.amountTooShort = "Le montant doit être superier à" +
+                " 0,50 €";
+            isValid = false
+        }
+
+        setAmountError(amountErr)
+        setCardNumberError(cardNumberErr)
+
+        return isValid
+    }
+
+    // const onSubmit = async values => {
+    //
+    //
+    //     console.log("INFORMATIONS PAYMENT", values.amount, values.libelle, values)
+    //     await sleep(300)
+    //     window.alert(JSON.stringify(values, 0, 2))
+    //     const requestOptions = {
+    //         method: 'PUT',
+    //         headers: { 'Content-Type': 'application/json' },
+    //         body: JSON.stringify(values)
+    //     };
+    //
+    //     await Axios.put('http://localhost:3000/api/payment', requestOptions)
+    //         .then(response => {
+    //             setIsPending(false)
+    //             this.state({amount: response.data.amount, libelle: response.data.libelle})
+    //         })
+    //         .catch( err => {
+    //             console.log(err)
+    //             setError(err.message)
+    //         })
+    // }
+
+    return (
+        <Container maxWidth="md">
+            <Box
+                display="flex"
+                flexDirection="column"
+                height="100%"
+                justifyContent="center"
+            >
+                <Container maxWidth="sm">
+                    <h1>NEXTPAL</h1>
+
+                    <form onSubmit={onSubmit}>
+                        <TextField type="text"
+                                   label="Numero de carte"
+                                   value={cardNumber}
+                                   variant="outlined"
+                                   fullWidth
+                                   helperText={Object.keys(cardNumberErr).map((key) => {
+                                       return <div key={key} style={{color: "red"}}>{cardNumberErr[key]}</div>
+                                   })
+                                   }
+                                   onChange={(e) => {setCardNumber(e.target.value)}}>
+                            CardNumber
+                        </TextField>
+                        <br/>
+                        <TextField type="text"
+                                   label="Montant"
+                                   value={amount}
+                                   margin="normal"
+                                   fullWidth
+                                   variant="outlined"
+                                   onChange={(e) => {setAmount(e.target.value)}}>
+                            Montant
+                        </TextField>
+                        <br/>
+                        {Object.keys(amountErr).map((key) => {
+                            return <div key={key} style={{color: "red"}}>{amountErr[key]}</div>
+                        })
+                        }
+                        <TextField type="text"
+                                   label="Raison"
+                                   margin="normal"
+                                   value={label}
+                                   fullWidth
+                                   variant="outlined"
+                                   onChange={(e) => {setLabel(e.target.value)}}>Paiement pour :</TextField>
+                        <br/>
+                        <Button
+                            color="primary"
+                            fullWidth
+                            margin="normal"
+                            size="large"
+                            type="submit"
+                            variant="contained">
+                            PAYER
+                        </Button>
+                        <br/>
+                        <Link href="/payments" margin="normal" component={Link}>
+                            Liste des paiements
+                        </Link>
+                    </form>
+                </Container>
+            </Box>
+        </Container>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -11,23 +11,25 @@ import {
 import Styles from "../components/shared/Styles";
 import {Button, makeStyles} from "@material-ui/core";
 import Axios from "axios";
+import PaymentView from "../components/shared/paymentForm";
+import Payment from "../components/shared/paymentForm";
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 // useEffect(() => {
-    // fetch('')
-    //     .then( res => {
-    //         if (!res.ok) {
-    //             throw Error('could not send the data')
-    //         }
-    //         return res.json();
-    //     })
-    //     .then( data => {
-    //         console.log(data)
-    //     })
-    //     .catch( err => {
-    //         console.log(err.message)
-    //     })
+// fetch('')
+//     .then( res => {
+//         if (!res.ok) {
+//             throw Error('could not send the data')
+//         }
+//         return res.json();
+//     })
+//     .then( data => {
+//         console.log(data)
+//     })
+//     .catch( err => {
+//         console.log(err.message)
+//     })
 // }, [])
 
 
@@ -60,112 +62,7 @@ export default function Home() {
             })
     }
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Payment Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          NextPal
-        </h1>
-          <Styles>
-              <Form
-                  onSubmit={onSubmit}
-                  render={({
-                               handleSubmit,
-                               form,
-                               submitting,
-                               pristine,
-                               values,
-                               active
-                           }) => {
-                      return (
-                          <form onSubmit={handleSubmit}>
-                              <Card
-                                  number={values.number || ''}
-                                  name={values.name || ''}
-                                  expiry={values.expiry || ''}
-                                  amount={values.amount || ''}
-                                  libelle={values.libelle || ''}
-                                  cvc={values.cvc || ''}
-                                  focused={active}
-                              />
-                              <div>
-                                  <Field
-                                      name="number"
-                                      component="input"
-                                      type="text"
-                                      pattern="[\d| ]{16,22}"
-                                      placeholder="Card Number"
-                                      format={formatCreditCardNumber}
-                                  />
-                              </div>
-                              <div>
-                                  <Field
-                                      name="name"
-                                      component="input"
-                                      type="text"
-                                      placeholder="Name"
-                                  />
-                              </div>
-                              <div>
-                                  <Field
-                                      name="expiry"
-                                      component="input"
-                                      type="text"
-                                      pattern="\d\d/\d\d"
-                                      placeholder="Valid Thru"
-                                      format={formatExpirationDate}
-                                  />
-                                  <Field
-                                      name="cvc"
-                                      component="input"
-                                      type="text"
-                                      pattern="\d{3,4}"
-                                      placeholder="CVC"
-                                      format={formatCVC}
-                                  />
-                              </div>
-                              <div>
-                                  <Field
-                                      name="libelle"
-                                      component="input"
-                                      type="text"
-                                      placeholder="Libelle"
-                                  />
-                                  <Field
-                                      name="amount"
-                                      component="input"
-                                      type="number"
-                                      placeholder="Amount"
-                                  />
-                              </div>
-                              <div>{ error && <div>{ error }</div>}</div>
-                              <div>{ isPending && <div>Sending...</div>}</div>
-                              <div className="buttons">
-                                  <Button color="primary" type="submit" disabled={submitting}>
-                                      Envoyer
-                                  </Button>
-                                  <button
-                                      type="button"
-                                      onClick={form.reset}
-                                      disabled={submitting || pristine}
-                                  >
-                                      Reset
-                                  </button>
-                              </div>
-                              <h2>Values</h2>
-                              <pre>{JSON.stringify(values, 0, 2)}</pre>
-                          </form>
-                      )
-                  }}
-              />
-          </Styles>
-        {/*<PaymentView/>*/}
-      </main>
-    </div>
-  )
+    return (
+        <Payment/>
+    )
 }
