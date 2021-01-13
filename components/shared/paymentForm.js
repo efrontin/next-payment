a1cf027fd16a71a2fca8dd248546ba20fb8c595f
Evrard FRONTIN <fe.moustic@gmail.fr>
2021-01-13 12:48:34 +0100

    [Payment Form] Update validation form

--- components/shared/paymentForm.js
+++ components/shared/paymentForm.js
@@ -22,45 +22,38 @@ export default function Payment() {
     const [amount, setAmount] = useState("")
     const [label, setLabel] = useState("")
 
-    const [isPending, setIsPending] = useState(false)
     const [amountErr, setAmountError] = useState({})
     const [cardNumberErr, setCardNumberError] = useState({})
-    const [error, setError] = useState(null)
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        const isValid = formValidation()
-        console.log()
-    }
-
-    const convertPrice = amount => {
-        (Math.round(amount * 1 * 100) / 100).toFixed(2);
-    }
+    // Gestion des validations
     const formValidation = () => {
+        console.log(cardNumber)
+        console.log(Number(cardNumber))
         const cardNumberErr = {}
         const amountErr = {}
         let isValid = true;
 
-        if (cardNumber.trim().length !== 16) {
+        if (cardNumber.replaceAll(' ', '').length !== 16) {
             cardNumberErr.cardNumberValidation = "Le nombre de numero sur" +
                 " la carte doit être de 16 chiffres";
             isValid = false
         }
 
-        // if (cardNumber.trim() !== toString()) {
-        //     cardNumberErr.cardNumberValidation = "Le numero de carte ne doit" +
-        //         " comporter que des chiffres";
-        //     isValid = false
-        // }
+        if (isNaN(Number(cardNumber.replaceAll(' ', '')))) {
+            cardNumberErr.cardNumberValidation = "Le numero de carte ne doit" +
+                " comporter que des chiffres";
+            isValid = false
+        }
 
-        if (amount.trim().length < 10000) {
+        if (amount > 10000) {
             amountErr.amountTooLong = "Le montant doit être inferieur à" +
                 " 10000 €";
             isValid = false
         }
 
-        if (amount.trim().length > 0.5) {
-            amountErr.amountTooShort = "Le montant doit être superier à" +
+        if (amount.replace(',', '.') < 0.5) {
+            // let coma = amount.replace(',', '.');
+            amountErr.amountTooShort = "Le montant doit être superieur à" +
                 " 0,50 €";
             isValid = false
         }
@@ -70,29 +63,54 @@ export default function Payment() {
 
         return isValid
     }
+    const handleCardNumberChanged = change => {
+        const cardFormat = change.replaceAll(" ", "")
+        if (cardFormat.length < 17 ) {
+            const addSpace = cardFormat.length !== 0 ? cardFormat.match(/.{1,4}/g).join(' ') : ''
+            console.log(addSpace)
+            setCardNumber(addSpace)
+        }
+    }
 
-    // const onSubmit = async values => {
-    //
-    //
-    //     console.log("INFORMATIONS PAYMENT", values.amount, values.libelle, values)
-    //     await sleep(300)
-    //     window.alert(JSON.stringify(values, 0, 2))
-    //     const requestOptions = {
-    //         method: 'PUT',
-    //         headers: { 'Content-Type': 'application/json' },
-    //         body: JSON.stringify(values)
-    //     };
-    //
-    //     await Axios.put('http://localhost:3000/api/payment', requestOptions)
-    //         .then(response => {
-    //             setIsPending(false)
-    //             this.state({amount: response.data.amount, libelle: response.data.libelle})
-    //         })
-    //         .catch( err => {
-    //             console.log(err)
-    //             setError(err.message)
-    //         })
-    // }
+
+    const handlerAmountChanged = digit => {
+        console.log(typeof digit)
+        // const convertDigit = parseFloat(digit)
+        setAmount(digit)
+    }
+
+    // Gestion de l'envoi du formulaire
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const isValid = formValidation()
+
+        if (isValid)
+        {
+            // Conversion du prix
+            const convertPrice = amount => (Math.round(amount * 1 * 100) / 100).toFixed(2)
+            console.log("INFORMATIONS PAYMENT", amount, amount)
+
+            const res = {
+                label: label,
+                amount: amount
+            }
+            console.log("INFORMATIONS PAYMENT", res)
+
+            // await sleep(300)
+            const requestOptions = {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: res
+            };
+
+            // POST de lq data
+            // Axios.post('http://localhost:3000/api/payment', requestOptions)
+            //      .catch( err => {
+            //          console.log(err)
+            //          setError(err.message)
+            //      })
+        }
+    }
 
     return (
         <Container maxWidth="md">
@@ -107,25 +125,29 @@ export default function Payment() {
 
                     <form onSubmit={onSubmit}>
                         <TextField type="text"
+                                   required
                                    label="Numero de carte"
                                    value={cardNumber}
                                    variant="outlined"
+                                   name="cardNumber"
                                    fullWidth
-                                   helperText={Object.keys(cardNumberErr).map((key) => {
-                                       return <div key={key} style={{color: "red"}}>{cardNumberErr[key]}</div>
-                                   })
-                                   }
-                                   onChange={(e) => {setCardNumber(e.target.value)}}>
+                                   onChange={(e) => {handleCardNumberChanged(e.target.value)}}>
                             CardNumber
                         </TextField>
                         <br/>
-                        <TextField type="text"
+                        {Object.keys(cardNumberErr).map((key) => {
+                            return <div key={key} style={{color: "red"}}>{cardNumberErr[key]}</div>
+                        })
+                        }
+                        <TextField type="number"
+                                   required
                                    label="Montant"
                                    value={amount}
                                    margin="normal"
+                                   name="amount"
                                    fullWidth
                                    variant="outlined"
-                                   onChange={(e) => {setAmount(e.target.value)}}>
+                                   onChange={(e) => {handlerAmountChanged(e.target.value)}}>
                             Montant
                         </TextField>
                         <br/>
@@ -135,13 +157,16 @@ export default function Payment() {
                         }
                         <TextField type="text"
                                    label="Raison"
+                                   required
                                    margin="normal"
                                    value={label}
+                                   name="label"
                                    fullWidth
                                    variant="outlined"
                                    onChange={(e) => {setLabel(e.target.value)}}>Paiement pour :</TextField>
                         <br/>
                         <Button
+                            disabled={!amount || !cardNumber || !label}
                             color="primary"
                             fullWidth
                             margin="normal"
